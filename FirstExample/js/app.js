58786986c0302aa48fb71bfc10f2a65d390ba815
Suomaa <a.antomonov@mail.ru>
2017-06-29 16:57:06 +0300

    add read more options and css change

--- FirstExample/js/app.js
+++ FirstExample/js/app.js
@@ -21,14 +21,41 @@ var my_news = [{
 ];
 
 var Article = React.createClass({
+    propTypes: {
+        lastNews: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+
+    getInitialState: function() {
+        return {
+            visible: false
+        };
+    },
+
+    readmoreClick: function(e) {
+        e.preventDefault();
+        this.setState({visible: true});
+    },
+
     render: function() {
         var author = this.props.lastNews.author,
-            text = this.props.lastNews.text;
+            text = this.props.lastNews.text,
+            bigText = this.props.lastNews.bigText,
+            visible = this.state.visible;
 
         return (
-            <div className="article">
-                <p className="news__author">{author}:</p>
-                <p className="news__text">{text}</p>
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className='news__text'>{text}</p>
+                <a href="#" 
+                    onClick={this.readmoreClick} 
+                    className={'news__readmore' + (visible ? ' none': '')}>
+                    Show more
+                </a>
+                <p className={'news__big-text' + (visible ? '': ' none')}>{bigText}</p>                
             </div>
         )
     }
