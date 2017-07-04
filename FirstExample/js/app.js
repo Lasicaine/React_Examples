fa48853641e3606d5316c49f5b9f430a9ab5ddce
Suomaa <a.antomonov@mail.ru>
2017-07-04 17:15:18 +0300

    Add News into View

--- FirstExample/js/app.js
+++ FirstExample/js/app.js
@@ -20,6 +20,8 @@ var my_news = [{
     }
 ];
 
+window.ee = new EventEmitter();
+
 var Article = React.createClass({
     propTypes: {
         lastNews: React.PropTypes.shape({
@@ -88,40 +90,119 @@ var News = React.createClass({
                 }
 });
 
-var TestInput = React.createClass({
+var Add = React.createClass({
 
-    onBtnClickHandler: function() {
-        alert(ReactDOM.findDOMNode(this.refs.myTestInput).value);
+  getInitialState: function() {
+    return {
+      agreeNotChecked: true,
+      authorIsEmpty: true,
+      textIsEmpty: true
+    };
+  },
+
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.author).focus();
+  },
+
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+    var textEl = ReactDOM.findDOMNode(this.refs.text);
+
+    var author = ReactDOM.findDOMNode(this.refs.author).value;
+    var text = textEl.value;
+
+    var item = [{
+        author: author,
+        text: text,
+        bigText: '...'
+    }];
+
+    window.ee.emit('News.add', item);
+
+    textEl.value = '';
+    this.setState({textIsEmpty: true});
+  },
+
+  onCheckRuleClick: function(e) {
+    this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+  },
+
+  onFieldChange: function(fieldName, e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({[''+fieldName]:false})
+    } else {
+      this.setState({[''+fieldName]:true})
+    }
+  },
+
+  render: function() {
+    var agreeNotChecked = this.state.agreeNotChecked,
+        authorIsEmpty = this.state.authorIsEmpty,
+        textIsEmpty = this.state.textIsEmpty;
+    return (
+      <form className='add cf'>
+        <input
+          type='text'
+          className='add__author'
+          onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+          placeholder='Ваше имя'
+          ref='author'
+        />
+        <textarea
+          className='add__text'
+          onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+          placeholder='Текст новости'
+          ref='text'
+        ></textarea>
+        <label className='add__checkrule'>
+          <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
+        </label>
+
+        <button
+          className='add__btn'
+          onClick={this.onBtnClickHandler}
+          ref='alert_button'
+          disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+          >
+          Add News
+        </button>
+      </form>
+    );
+  }
+});
+
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            news: my_news
+        };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+        window.ee.addListener('News.add', function(item) {
+            var nextNews = item.concat(self.state.news);
+            self.setState({news: nextNews});
+        });
+    },
+
+    componentWillUnmount: function() {
+        window.ee.removeListener('News.add');
     },
 
     render: function() {
-        return (
-            <div>
-                <input
-                    className='test-input'
-                    defaultValue=''
-                    placeholder='Enter value'
-                    ref='myTestInput'
-                />
-                <button onClick={this.onBtnClickHandler}>Open alert</button>
+        return ( 
+            <div className = "app">
+                <Add />
+                <h1>News</h1>
+                <News lastNews = {this.state.news} />
             </div>
         );
     }
 });
 
-var App = React.createClass({
-            render: function() {
-                return ( 
-                <div className = "app">
-                    <h1>News</h1>
-                    <TestInput />
-                    <News lastNews = {my_news} /> {/* Add data property */} 
-                </div>
-                );
-            }
-        });
-
-        ReactDOM.render( 
-            <App />,
-            document.getElementById('root')
-        );
+ReactDOM.render( 
+    <App />,
+    document.getElementById('root')
+);
